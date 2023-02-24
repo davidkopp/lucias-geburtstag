51f265885f72df1b1bb0d7859bf94ee432cd3511
David Kopp <mail@davidkopp.de>
2023-02-24 17:58:39 +0100

    Change URL

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ echo "Building Zola site"
 zola build
 
 echo "Sending to remote"
-rsync -ru public/. david@dev.davidkopp.de:/var/www/lucias-geburtstag --delete
+rsync -ru public/. david@luciahoerner.de:/var/www/lucias-geburtstag --delete
